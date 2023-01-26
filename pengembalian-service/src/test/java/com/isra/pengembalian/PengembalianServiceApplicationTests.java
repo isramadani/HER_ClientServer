ddabfe79fc084718521a03ad1b2691ba1682cac1
isramadani <113502867+isramadani@users.noreply.github.com>
2023-01-26 14:11:53 +0700

    Add files via upload

---
+++ pengembalian-service/src/test/java/com/isra/pengembalian/PengembalianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isra.pengembalian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengembalianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
