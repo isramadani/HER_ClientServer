f5b7ca36762091b73994c87fd20556a02ae7774b
isramadani <113502867+isramadani@users.noreply.github.com>
2023-01-26 14:09:26 +0700

    Add files via upload

---
+++ nasabah-service/src/test/java/com/isra/nasabah/NasabahServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isra.nasabah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasabahServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
